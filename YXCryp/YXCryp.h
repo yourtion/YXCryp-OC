7356d2622bc8c17dc036bc30518b53e0f6abab6b
YourtionGuo <yourtion@gmail.com>
2016-08-12 22:18:16 +0800

    Initial Commit

---
+++ YXCryp/YXCryp.h
@@ -0,0 +1,19 @@
+//
+//  YXCryp.h
+//  YXCryp
+//
+//  Created by YourtionGuo on 8/12/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YXCryp.
+FOUNDATION_EXPORT double YXCrypVersionNumber;
+
+//! Project version string for YXCryp.
+FOUNDATION_EXPORT const unsigned char YXCrypVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXCryp/PublicHeader.h>
+
+
